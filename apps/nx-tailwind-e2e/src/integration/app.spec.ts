6cd60301323f78d425c59e5206e6053a711f13ad
atrifyllis <atrifyllis@gmail.com>
2021-01-24 22:24:10 +0200

    improve e2e test

--- apps/nx-tailwind-e2e/src/integration/app.spec.ts
+++ apps/nx-tailwind-e2e/src/integration/app.spec.ts
@@ -1,13 +1,14 @@
-import { getGreeting } from '../support/app.po';
+import { gerPrimaryButton, getGreeting } from '../support/app.po';
 
 describe('nx-tailwind', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
     // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    // cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
     getGreeting().contains('TailwindCSS');
+    gerPrimaryButton().contains("Primary Button")
   });
 });
--- apps/nx-tailwind-e2e/src/support/app.po.ts
+++ apps/nx-tailwind-e2e/src/support/app.po.ts
@@ -1,1 +1,3 @@
 export const getGreeting = () => cy.get('h1 span');
+
+export const gerPrimaryButton = () => cy.get('.mat-primary');
