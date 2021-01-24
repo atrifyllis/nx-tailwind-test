b06582eaadf5245fc55be2d253915bac3c21d4fb
atrifyllis <atrifyllis@gmail.com>
2021-01-24 22:17:18 +0200

    make cypress working

--- apps/nx-tailwind-e2e/src/integration/app.spec.ts
+++ apps/nx-tailwind-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('nx-tailwind', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to nx-tailwind!');
+    getGreeting().contains('TailwindCSS');
   });
 });
