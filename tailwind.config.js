89647cadb61f2140cb17215cf336b0781c7fae41
Alexandros Trifyllis <alexandros.trifyllis@trasys.gr>
2021-03-07 11:35:17 +0200

    fix slow re-compilation time

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   important: true,
   prefix: '',
   purge: {
+    enabled: true,
     content: ['./apps/**/*.{html,ts}', './libs/**/*.{html,ts}'],
   },
   darkMode: false, // or 'media' or 'class'
